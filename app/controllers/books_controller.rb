7e275a9e44be90e20ed4190d78e30b9e3637b8fd
Kei Fukuyori <ocvxkeikei@gmail.com>
2016-11-26 05:28:44 +0000

    新規追加機能追加、編集機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,30 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
-  def new
-    
+  def new 
+    @book = Book.new
   end
   
-  def create 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:name,:price,:publish,:publish_date)
   end
 end
